6142893558579843edaffc45c242042d3233672a
Timo Lehtonen <timo.lehtonen@gmail.com>
2015-10-20 06:06:37 +0300

    Use Pandas

--- analyze-experiment.py
+++ analyze-experiment.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python
 
-import sys, re, datetime, urllib, socket
+import sys, re
+
+import numpy as np
+import pandas as pd
 
 print
 print "Analyzing experiment data."
 print
 print "Usage: <lang> <inputfile> <session threshold>"
 print
+# cat event_dump2.txt | sed 's/[^ ]|/;/g' | sed 's/[ ]*|/|/g' | sed 's/|[ ]*/|/g' | sed 's/^ *//' | grep -v '^[-(]' | grep -v '^$' > event_dump2.csv
 
 inputFileName = sys.argv[1]
 
-debug = False
+print "Input file: " + inputFileName
 
-print "Reading file" + inputFileName
+#data = np.recfromcsv(inputFileName, delimiter='|')
+#with open(inputFileName, 'rb') as csvFile:
+#  csvReader = csv.reader(csvFile, delimiter='|')
+#  for row in csvReader:
+#    print ', '.join(row)
 
-inputFile = open(inputFileName, "r")
+print "Read CSV file..."
 
-lines = 0
+df = pd.read_csv(inputFileName, sep='|')
 
-for line in inputFile:
+print "Done."
 
-  if debug:
-    print "Line: " + line
-
-  lines = lines + 1
-
-print "Read " + str(lines) + " lines."
+print "There are {:.0f} rows in the data.".format(len(df))
